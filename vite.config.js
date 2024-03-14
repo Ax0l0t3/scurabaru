0407911bfedd93efe66635cf4cd0129e89ee6e30
D4v3 <antenopolis@gmail.com>
2024-03-14 16:58:54 -0600

    To scurabaru

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Ax0l0t3.github.io/scuraburu'
+  base: '/scurabaru/'
 })
